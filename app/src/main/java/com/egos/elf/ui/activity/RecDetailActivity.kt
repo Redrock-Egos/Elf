db8f3bbe4176c701f23e69b0f23e2018fce7faca
Fxymine4ever <381391264@qq.com>
2018-11-26 23:57:26 +0800

    日推等着播放界面

--- app/src/main/java/com/egos/elf/ui/activity/RecDetailActivity.kt
+++ app/src/main/java/com/egos/elf/ui/activity/RecDetailActivity.kt
@@ -4,18 +4,15 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.egos.elf.R
-import com.egos.elf.R.id.rec_detail_rv
 import com.egos.elf.common.base.BaseNoNeedListenActivity
 import com.egos.elf.common.bean.moe.PlayListWrapper
 import com.egos.elf.common.net.ApiGenerator
-import com.egos.elf.common.utils.setSchedulers
 import com.egos.elf.common.utils.withSPCache
 import com.egos.elf.ui.adapter.RecDetailAdapter
 import kotlinx.android.synthetic.main.activity_rec_detail.*
 
 class RecDetailActivity : BaseNoNeedListenActivity() {
     private val moeService = ApiGenerator.getMoeApiService()
-    private lateinit var wrapper: PlayListWrapper
     private lateinit var mAdapter: RecDetailAdapter
 
 
@@ -28,21 +25,20 @@ class RecDetailActivity : BaseNoNeedListenActivity() {
         super.onCreate(savedInstanceState)
         common_toolbar.init("")
         getBundleData()
-        //App.playListManager.updatePlayList(PlayListManager.DEFAULT_PLAY_LIST_KEY, wrapper.result)
     }
 
     private fun getBundleData() {
         intent.extras?.let { it ->
             id = it.getLong("albumId")
-            Log.d("fxy",id.toString())
+            Log.d("fxy", id.toString())
             withSPCache(
                     id.toString(),
                     PlayListWrapper::class.java,
                     { moeService.getPlayList(id) },
                     { wrapper ->
-                        Log.d("fxy", wrapper.toString())
-                        this.wrapper = wrapper
+                        rec_detail_tv_title.text = wrapper.result.name
                         initView(wrapper)
+                        mAdapter.setWrapper(wrapper)
                     })
         }
     }
